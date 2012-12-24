2a10e003175e62783948792703d21dc38d57b711
Sergey Pepyakin <frightinside@gmail.com>
2012-12-24 18:37:17 +0400

    Реализованы модели для транзакции и списка транзакций.

---
+++ src/main/java/com/ideasium/btcej/Transaction.java
@@ -0,0 +1,84 @@
+/*
+ * ${NAME}
+ * ${PROJECT_NAME}
+ *
+ * Created by ${USER} on ${DAY}.${MONTH}.${YEAR}.
+ * Copyright (c) ${YEAR} ideasium. All rights reserved.
+ */
+
+package com.ideasium.btcej;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+import com.ideasium.btcej.common.Currency;
+
+/**
+* @author pepyakin
+*/
+public class Transaction {
+
+    @JsonProperty("type")
+    private int type;
+
+    @JsonProperty("amount")
+    private double amount;
+
+    @JsonProperty("currency")
+    private Currency currency;
+
+    @JsonProperty("desc")
+    private String description;
+
+    @JsonProperty("status")
+    private int status;
+
+    @JsonProperty("timestamp")
+    private long timestamp;
+
+    public int getType() {
+        return type;
+    }
+
+    public void setType(int type) {
+        this.type = type;
+    }
+
+    public double getAmount() {
+        return amount;
+    }
+
+    public void setAmount(double amount) {
+        this.amount = amount;
+    }
+
+    public Currency getCurrency() {
+        return currency;
+    }
+
+    public void setCurrency(Currency currency) {
+        this.currency = currency;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public int getStatus() {
+        return status;
+    }
+
+    public void setStatus(int status) {
+        this.status = status;
+    }
+
+    public long getTimestamp() {
+        return timestamp;
+    }
+
+    public void setTimestamp(long timestamp) {
+        this.timestamp = timestamp;
+    }
+}
